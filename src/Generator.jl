0c969ced03f7024acd3ca8c98920b396a54c1a2e
WhiffleFish <tylertronics@gmail.com>
2021-06-09 20:49:04 -0400

    sizehint -> undef vec

--- src/Generator.jl
+++ src/Generator.jl
@@ -3,7 +3,7 @@ function GenBelief(rng::AbstractRNG, pomdp::POMDP{S,A,O}, b::WeightedParticleBel
     N = n_particles(b)
     weighted_return = 0.0
 
-    bp = WeightedParticleBelief(sizehint!(S[],N), sizehint!(Float64[],N))
+    bp = WeightedParticleBelief(Vector{S}(undef, N), Vector{Float64}(undef, N))
 
     p_idx = StatsBase.sample(StatsBase.weights(b.weights))
     sample_obs = nothing
@@ -11,7 +11,8 @@ function GenBelief(rng::AbstractRNG, pomdp::POMDP{S,A,O}, b::WeightedParticleBel
     sample_s = particle(b, p_idx)
     if isterminal(pomdp, sample_s)
         w = inv(N)
-        for _ in 1:N; push!(bp.particles,sample_s); push!(bp.weights, w); end
+        bp.particles .= sample_s
+        bp.weights .=  w
         sample_obs = first(Vector{O}(undef,1)) # Random Observation
         weighted_return = 0.0
         return bp::WeightedParticleBelief{S}, sample_obs::O, weighted_return::Float64
@@ -29,7 +30,7 @@ function GenBelief(rng::AbstractRNG, pomdp::POMDP{S,A,O}, b::WeightedParticleBel
             end
         end
 
-        push!(bp.particles, sp)
+        bp.particles[i] = sp
 
         weighted_return += r*w
     end
@@ -39,7 +40,7 @@ function GenBelief(rng::AbstractRNG, pomdp::POMDP{S,A,O}, b::WeightedParticleBel
         s = particle(b, i)
         sp = particle(bp, i)
         w = weight(b, i)
-        push!(bp.weights, w*pdf(POMDPs.observation(pomdp, s, a, sp), sample_obs))
+        bp.weights[i] = w*pdf(POMDPs.observation(pomdp, s, a, sp), sample_obs)
     end
 
     if !iszero(sum(bp.weights))
