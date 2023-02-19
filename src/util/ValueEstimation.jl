5c005e4e6b4729817475ee1e9fec1992ceee3a1f
WhiffleFish <tylertronics@gmail.com>
2023-02-19 13:55:54 -0700

    type stable rollout depth

--- src/util/ValueEstimation.jl
+++ src/util/ValueEstimation.jl
@@ -38,12 +38,12 @@ struct FastRandomSolver
     d::Union{Nothing, Int}
 end
 
-FastRandomSolver(d=10) = FastRandomSolver(Random.default_rng(), d)
+FastRandomSolver(d=nothing) = FastRandomSolver(Random.default_rng(), d)
 
 struct FastRandomRolloutEstimator{ObsRequired, A, RNG <:AbstractRNG}
     actions::A
     rng::RNG
-    d::Union{Nothing,Int}
+    d::Union{Nothing, Int}
 end
 
 function FastRandomRolloutEstimator(pomdp::POMDP, estim::FastRandomSolver, obs_req::Bool)
@@ -59,34 +59,30 @@ const ObsReqFRRE = FastRandomRolloutEstimator{true, A, RNG} where {A,RNG}
 POMDPs.action(p::FastRandomRolloutEstimator, ::Any) = rand(p.rng, p.actions)
 
 function MCTS.convert_estimator(estimator::FastRandomSolver, ::Any, pomdp::POMDP)
-
-    obs_req = is_obs_required(pomdp)
-
-    return FastRandomRolloutEstimator(pomdp, estimator, obs_req)
+    return FastRandomRolloutEstimator(pomdp, estimator, is_obs_required(pomdp))
 end
 
-function sr_gen(estimator::FastRandomRolloutEstimator{false}, pomdp::POMDP{S,A,O}, s::S, a::A) where {S,A,O}
+function sr_gen(estimator::FastRandomRolloutEstimator{false}, pomdp::POMDP{S,A}, s::S, a::A) where {S,A}
     return sr_gen(Val(false), estimator.rng, pomdp, s, a)
 end
 
-function sr_gen(estimator::FastRandomRolloutEstimator{true}, pomdp::POMDP{S,A,O}, s::S, a::A) where {S,A,O}
+function sr_gen(estimator::FastRandomRolloutEstimator{true}, pomdp::POMDP{S,A}, s::S, a::A) where {S,A}
     return sr_gen(Val(true), estimator.rng, pomdp, s, a)
 end
 
-function sr_gen(::Val{false}, rng::AbstractRNG, pomdp::POMDP{S,A,O}, s::S, a::A) where {S,A,O}
+function sr_gen(::Val{false}, rng::AbstractRNG, pomdp::POMDP{S,A}, s::S, a::A) where {S,A}
     sp = rand(rng, transition(pomdp, s, a))
     r = reward(pomdp, s, a, sp)
     return sp, r
 end
 
-function sr_gen(::Val{true}, rng::AbstractRNG, pomdp::POMDP{S,A,O}, s::S, a::A) where {S,A,O}
+function sr_gen(::Val{true}, rng::AbstractRNG, pomdp::POMDP{S,A}, s::S, a::A) where {S,A}
     return @gen(:sp,:r)(pomdp, s, a, rng)
 end
 
-function MCTS.estimate_value(estimator::FastRandomRolloutEstimator, pomdp::POMDP{S,A,O}, s::S, depth::Int) where {S,A,O}
+function MCTS.estimate_value(estimator::FastRandomRolloutEstimator, pomdp::POMDP{S}, s::S, max_depth::Int) where S
 
     disc = 1.0
-    max_depth = isnothing(estimator.d) ? depth : estimator.d
     r_total = 0.0
     rng = estimator.rng
     step = 1
@@ -111,8 +107,9 @@ end
 # Fallback for non-belief defined value estimators
 function MCTS.estimate_value(est, pomdp::POMDP{S}, b::PFTBelief{S}, d::Int) where S
     v = 0.0
+    max_depth = isnothing(est.d) ? d : est.d
     for (s,w) in weighted_particles(b)
-        v += w*MCTS.estimate_value(est, pomdp, s, d) # estim def in terms of state
+        v += w*MCTS.estimate_value(est, pomdp, s, max_depth::Int)
     end
     return v
 end
@@ -189,18 +186,18 @@ end
 
 function partial_rollout(est::ParticleFilterTrees.SolvedPORollout, pomdp::POMDP{S}, b::ParticleCollection{S}, d::Int) where S
     v = 0.0
+    max_depth = isnothing(est.d) ? d : est.d
     b_ = est.rb
     for _ in 1:est.n_rollouts
         b_.particles .= est.ib.particles
         s = rand(est.rng, b)
-        v += rollout(est, pomdp, b_, s, d)
+        v += rollout(est, pomdp, b_, s, max_depth::Int)
     end
     return v/est.n_rollouts
 end
 
-function rollout(est::ParticleFilterTrees.SolvedPORollout, pomdp::POMDP{S}, b::ParticleCollection{S}, s::S, d::Int) where S
+function rollout(est::ParticleFilterTrees.SolvedPORollout, pomdp::POMDP{S}, b::ParticleCollection{S}, s::S, max_depth::Int) where S
     updater = est.updater
-    max_depth = isnothing(est.d) ? d : est.d
     rng = est.rng
     policy = est.policy
 
